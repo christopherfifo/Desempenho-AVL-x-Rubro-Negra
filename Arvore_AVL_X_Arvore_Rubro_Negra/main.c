74d8395879e2efb3f6d2fd19f667dba0dfa50308
GabrielVitorGL <gabrielvitorcom@hotmail.com>
2025-06-26 02:14:59 -0300

    fixing errors

--- Arvore_AVL_X_Arvore_Rubro_Negra/main.c
+++ Arvore_AVL_X_Arvore_Rubro_Negra/main.c
@@ -4,32 +4,34 @@
 #include <sys/time.h>
 #include "auxiliar.h"
 
-
 int main()
 {
     FILE *arquivo = fopen("massaDados.csv", "r");
-    if (arquivo == NULL) {
+    if (arquivo == NULL)
+    {
         printf("\nErro ao abrir o arquivo\n\n");
         exit(1);
     }
     fclose(arquivo);
 
     FILE *criando = fopen("funcionarios_ordenados.csv", "r");
-    if (criando == NULL) {
+    if (criando == NULL)
+    {
         printf("Aquivo nao existe\n");
         printf("\nEstamos criando o vetor ordenado!\n\n");
-        alimenta_arvore(CRIANDO_ARVORE, "massaDados.csv", 0);
-    } else {
+        alimenta_arvore(CRIANDO_ARVORE, "massaDados.csv");
+    }
+    else
+    {
         printf("Arquivo funcionarios_ordenados.csv ja existe!\n");
         fclose(criando);
     }
 
-
     printf("\nIniciando o programa...\n\n");
-    alimenta_arvore(ARVORE_AVL, "massaDados.csv", 1);
-    alimenta_arvore(ARVORE_RUBRO_NEGRA, "massaDados.csv", 1);
-    alimenta_arvore(ARVORE_AVL, "funcionarios_ordenados.csv", 1);
-    alimenta_arvore(ARVORE_RUBRO_NEGRA, "funcionarios_ordenados.csv", 1);
+    alimenta_arvore(ARVORE_AVL, "massaDados.csv");
+    alimenta_arvore(ARVORE_RUBRO_NEGRA, "massaDados.csv");
+    alimenta_arvore(ARVORE_AVL, "funcionarios_ordenados.csv");
+    alimenta_arvore(ARVORE_RUBRO_NEGRA, "funcionarios_ordenados.csv");
 
     exibeTempos();
 
